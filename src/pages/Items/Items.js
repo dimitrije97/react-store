08e4bcbe00cba331c9d59cb0bc974968aa052c70
DamjanPantic <panticdamjan@gmail.com>
2021-03-21 20:10:23 +0100

    Items view

---
+++ src/pages/Items/Items.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import { Container, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux'
+import Item from '../../components/Item/Item';
+import { setItems } from '../../redux/actions';
+import WarehouseService from '../../services/warehouse-service';
+
+import './Items.css'
+
+function Items() {
+
+    const dispatch = useDispatch();
+
+    const warehouseStore = useSelector(state => state.warehouseStore);
+
+    useEffect(() => {
+        WarehouseService.getItems().then(res => {
+            dispatch(setItems(res.data))
+        })
+    }, [])
+
+    const allItems = warehouseStore.items ? warehouseStore.items.map(item => <Item item={item} key={item.IDRecept} />) : null;
+
+    return (
+        <div className="bg">
+            <Row className="justify-content-center">
+                {allItems}
+                {allItems}
+                {allItems}
+                {allItems}
+            </Row>
+        </div>
+    )
+}
+
+export default Items
